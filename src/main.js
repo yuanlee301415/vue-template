9185996ae46a4ecfc86d352fef7ba6a70fad0c42
yuanlee <yuanlee@163.com>
2022-04-23 21:00:24 +0800

    installed & first run.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
 import App from './App.vue'
 import router from './router'
 import '@/assets/style.css'
-import '@/assets/g6.css'
 
 Vue.config.productionTip = false
-Vue.use(Element)
-Vue.prototype.$ELEMENT = {size: 'small'}
 
 new Vue({
   el: '#app',
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,11 @@
 <template>
     <div id="home" style="text-align: center;">
-        <img alt="Vue logo" src="@/assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" year="2020" month="4" date="21" class="hello"/>
+      Home
     </div>
 </template>
 
 <script>
-  import HelloWorld from '@/components/HelloWorld.vue'
-
   export default {
-    name: 'Home',
-    components: {
-      HelloWorld
-    }
+    name: 'Home'
   }
 </script>
